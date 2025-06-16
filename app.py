b6acc955a313bd7605e4280ef9894cc32b69c9e0
Alok Maurya <47533834+alokmauryajnp@users.noreply.github.com>
2025-06-16 12:32:13 +0530

    app.py

--- app.py
+++ app.py
@@ -11,19 +11,19 @@ app = Flask(__name__)
 def home():
     return "Smile enhancer is live!"
 
-# def process_image():
-#     file = request.files['image']
-#     img = Image.open(file.stream).convert('RGB')
-#     img_np = np.array(img)
+def process_image():
+    file = request.files['image']
+    img = Image.open(file.stream).convert('RGB')
+    img_np = np.array(img)
 
-#     # Whitening logic: increase brightness of full image (demo)
-#     hsv = cv2.cvtColor(img_np, cv2.COLOR_RGB2HSV)
-#     hsv[:, :, 2] = cv2.add(hsv[:, :, 2], 30)
-#     result = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+    # Whitening logic: increase brightness of full image (demo)
+    hsv = cv2.cvtColor(img_np, cv2.COLOR_RGB2HSV)
+    hsv[:, :, 2] = cv2.add(hsv[:, :, 2], 30)
+    result = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
-#     result_img = Image.fromarray(result)
-#     buffer = BytesIO()
-#     result_img.save(buffer, format="PNG")
-#     buffer.seek(0)
+    result_img = Image.fromarray(result)
+    buffer = BytesIO()
+    result_img.save(buffer, format="PNG")
+    buffer.seek(0)
 
-#     return send_file(buffer, mimetype='image/png')
+    return send_file(buffer, mimetype='image/png')
